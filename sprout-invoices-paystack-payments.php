db1bb5054e883397166deb9fffc33101166c7f39
Dan Cameron <dan@sproutapps.co>
2019-01-08 10:54:04 -0800

    Compatible with Sprout Invoices v19+

--- sprout-invoices-paystack-payments.php
+++ sprout-invoices-paystack-payments.php
@@ -11,25 +11,15 @@ Author URI: https://paystack.com
 /**
  * Plugin File
  */
-define('SI_ADDON_PAYSTACK_VERSION', '3.1');
-define('SI_ADDON_PAYSTACK_DOWNLOAD_ID', 141);
-define('SI_ADDON_PAYSTACK_FILE', __FILE__);
-define('SI_ADDON_PAYSTACK_NAME', 'Sprout Invoices Paystack Payments');
-define('SI_ADDON_PAYSTACK_URL', plugins_url('', __FILE__));
+define( 'SI_ADDON_PAYSTACK_VERSION', '3.1' );
+define( 'SI_ADDON_PAYSTACK_DOWNLOAD_ID', 141 );
+define( 'SI_ADDON_PAYSTACK_FILE', __FILE__ );
+define( 'SI_ADDON_PAYSTACK_NAME', 'Sprout Invoices Paystack Payments' );
+define( 'SI_ADDON_PAYSTACK_URL', plugins_url( '', __FILE__ ) );
 
 
 // Load up the processor before updates
-add_action('si_payment_processors_loaded', 'si_load_paystack');
-function si_load_paystack() 
-{
-    include_once 'SI_Paystack.php';
-}
-
-// Load up the updater after si is completely loaded
-add_action('sprout_invoices_loaded', 'si_load_paystack_updates');
-function si_load_paystack_updates() 
-{
-    if (class_exists('SI_Updates') ) {
-        include_once 'SI_Updates.php';
-    }
+add_action( 'si_payment_processors_loaded', 'si_load_paystack' );
+function si_load_paystack() {
+	include_once 'SI_Paystack.php';
 }
